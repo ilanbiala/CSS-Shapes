dfff922ccc9fb205c68d5b0c7b3194c238580596
Ilan Biala <ilan.biala@gmail.com>
2014-02-03 16:26:04 -0500

    some editing for attributes enabled

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,20 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+	$('em[contenteditable="true"]').on('keyup', function(e) {
+		if (e.which == 13 || e.which == 27) {
+			$(this).trigger('blur');
+			return;
+		}
+		var elem = $(this);
+		var propToChange = elem.attr('data-css');
+		propToChange ? console.log('gonna change stuff') : console.log('no css to change'); return;
+		var elemToChange = elem.parent().parent().prev().children();
+		if (propToChange == 'transform') {
+			console.log(propToChange, 'skew(' + elem.text() + ')');
+			elemToChange.css('-webkit-' + propToChange, 'skew(' + elem.text() + ')').css('-moz-' + propToChange, 'skew(' + elem.text() + ')').css('-ms-' + propToChange, 'skew(' + elem.text() + ')').css('-o-' + propToChange, 'skew(' + elem.text() + ')').css(propToChange, 'skew(' + elem.text() + ')');
+		}
+		elemToChange.css(propToChange, elem.text());
+		if (elem.attr('data-css-two') !== undefined) {
+			elemToChange.css(elem.attr('data-css-two'), elem.text());
+		}
+	});
+});
